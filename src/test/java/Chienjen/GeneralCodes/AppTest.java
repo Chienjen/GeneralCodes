8740a4b26831e94a119147e13f3d5d0d36f69213
Chienjen <nithicksharma@gmail.com>
2017-10-11 17:55:21 +0530

    ReCreate project

--- src/test/java/Chienjen/GeneralCodes/AppTest.java
+++
@@ -1,8 +0,0 @@
-package Chienjen.GeneralCodes;
-
-public class AppTest {
-	public static void main(String[] args) {
-		System.out.println( "Hello World1" );
-        System.out.println( "Hello World2" );
-	}
-}
