8e134cab89a1b6a2236281b755c64e268bc7d04b
Chienjen <nithicksharma@gmail.com>
2017-10-11 17:50:40 +0530

    Project created

---
+++ src/test/java/Chienjen/GeneralCodes/AppTest.java
@@ -0,0 +1,8 @@
+package Chienjen.GeneralCodes;
+
+public class AppTest {
+	public static void main(String[] args) {
+		System.out.println( "Hello World1" );
+        System.out.println( "Hello World2" );
+	}
+}
